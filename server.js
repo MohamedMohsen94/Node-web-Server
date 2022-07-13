b6e156c2907d5ed832f127e74b2b21b25805c6bd
Mohsen <m.mohsen28894@gmail.com>
2022-07-13 16:27:49 +0200

    anew page added

--- server.js
+++ server.js
@@ -45,6 +45,12 @@ app.get("/about", (req, res) => {
   });
 });
 
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    Title: "projects",
+  });
+});
+
 app.get("/bad", (req, res) => {
   res.send({
     errorMsg: "Unable to handle request",
